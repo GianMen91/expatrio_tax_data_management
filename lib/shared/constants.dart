e7617c6e5111310745d791a08ecdffd70896a712
GianMen91 <giancarlo.mennillo@gmail.com>
2024-01-25 10:22:36 +0100

    Initial commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,8 @@
+// Import the Flutter material library
+import 'package:flutter/material.dart';
+
+// Define color constants for text and background
+const themeColor = Color.fromRGBO(65, 171, 158, 1);
+
+// Define a constant for default padding
+const defaultPadding = 20.0;
