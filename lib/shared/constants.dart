1bbd99980b2e2447f2187a3baa9e1514648e6f79
GianMen91 <giancarlo.mennillo@gmail.com>
2024-01-25 17:18:52 +0100

    add errorMessageColor constant

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 // Define color constants for text and background
 const themeColor = Color.fromRGBO(65, 171, 158, 1);
+const errorMessageColor = Color(0xFFFEC008);
 
 // Define a constant for default padding
 const defaultPadding = 20.0;
