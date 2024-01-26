9255e6089b9adcbf6621af9b135997f4373f8b9b
GianMen91 <giancarlo.mennillo@gmail.com>
2024-01-26 11:01:40 +0100

    create model Tax Residence

---
+++ lib/models/TaxResidence.dart
@@ -0,0 +1,15 @@
+// Import the Flutter material library
+import 'package:flutter/material.dart';
+
+// Define a class representing a TaxResidence item
+class TaxResidence {
+  // Properties of a TaxResidence item
+  final String country;
+  final String id;
+
+  // Constructor for creating a TaxResidence item
+  TaxResidence({
+    required this.country,
+    required this.id,
+  });
+}
