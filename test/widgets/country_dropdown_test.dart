77fe840d6e69b4ab3223d0d584e69926ca470607
GianMen91 <giancarlo.mennillo@gmail.com>
2024-01-28 18:15:19 +0100

    update tests

--- test/widgets/country_dropdown_test.dart
+++ test/widgets/country_dropdown_test.dart
@@ -1,3 +1,4 @@
+import 'package:coding_challenge/models/tax_residence.dart';
 import 'package:coding_challenge/widgets/country_dropdown.dart';
 import 'package:coding_challenge/widgets/search_box.dart';
 import 'package:flutter/material.dart';
@@ -5,37 +6,35 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('CountryDropdown tests', () {
-    late CountryDropdown countryDropdown;
+    CountryDropdown countryDropdown = CountryDropdown(
+      updateSelectedCountry: (value) {},
+      index: 1,
+      validateCountry: const [false, false],
+      taxResidences: [
+        TaxResidence(country: "AD", id: "12bb"),
+        TaxResidence(country: "AL", id: "fffg")
+      ],
+      selectedCountryLabel: 'Albania',
+    );
 
-    setUp(() {
-      countryDropdown = CountryDropdown(
-        updateSelectedCountry: (value) {},
-        index: 0,
-        validateCountry: [false],
-        taxResidences: [],
-        selectedCountryLabel: 'Select Country',
-      );
-    });
+    testWidgets('UI components are rendered correctly',
+            (WidgetTester tester) async {
+          await tester.pumpWidget(MaterialApp(home: countryDropdown));
 
-    testWidgets('UI components are rendered correctly', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: countryDropdown));
+          // Tap the container to open the bottom sheet
+          await tester.tap(find.byType(GestureDetector));
+          await tester.pumpAndSettle();
 
-      expect(find.text('Select Country'), findsOneWidget);
-      expect(find.byType(SearchBox), findsOneWidget);
-      expect(find.byType(ListTile), findsNothing); // List of countries is initially empty
-    });
+          var countryDropdownTitle =
+          find.byKey(const Key('country_dropdown_title'));
+          expect(countryDropdownTitle, findsOneWidget);
 
-    testWidgets('Tap opens bottom sheet for country selection', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: countryDropdown));
+          final countryDropdownTitleWidget =
+          tester.widget<Text>(countryDropdownTitle);
+          expect(countryDropdownTitleWidget.data, "Country");
 
-      // Tap the container to open the bottom sheet
-      await tester.tap(find.byType(GestureDetector));
-      await tester.pumpAndSettle();
-
-      expect(find.text('Country'), findsOneWidget);
-      expect(find.byType(SearchBox), findsOneWidget);
-      expect(find.byType(ListTile), findsNothing); // List of countries is initially empty
-    });
-
+          expect(find.byType(SearchBox), findsOneWidget);
+          expect(find.byKey(const Key('country_list_view')), findsOneWidget);
+        });
   });
 }
