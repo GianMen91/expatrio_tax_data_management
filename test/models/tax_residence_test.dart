532d3f34620e28e565fdb8fa8825a85aac99724b
GianMen91 <giancarlo.mennillo@gmail.com>
2024-01-28 16:40:00 +0100

    update tax_residence_test.dart

--- test/models/tax_residence_test.dart
+++ test/models/tax_residence_test.dart
@@ -2,37 +2,16 @@ import 'package:coding_challenge/models/tax_residence.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('TaxResidence tests', () {
-    test('Two identical instances should be equal', () {
-      final taxResidence1 = TaxResidence(country: 'USA', id: '123');
-      final taxResidence2 = TaxResidence(country: 'USA', id: '123');
+  group('TaxResidence', () {
+    test('Constructor should initialize properties correctly', () {
+      // Arrange
+      const String country = 'AF';
+      const String id = '1234';
 
-      expect(taxResidence1, equals(taxResidence2));
-    });
+      final TaxResidence taxResidence = TaxResidence(country: country, id: id);
 
-    test('Instances with different properties should not be equal', () {
-      final taxResidence1 = TaxResidence(country: 'USA', id: '123');
-      final taxResidence2 = TaxResidence(country: 'Canada', id: '456');
-
-      expect(taxResidence1, isNot(equals(taxResidence2)));
-    });
-
-    test('Changing a property should not affect equality', () {
-      final taxResidence1 = TaxResidence(country: 'USA', id: '123');
-      final taxResidence2 = TaxResidence(country: 'USA', id: '123');
-
-      taxResidence2.country = 'Canada';
-
-      expect(taxResidence1, equals(taxResidence2));
-    });
-
-    test('toString should return a meaningful representation', () {
-      final taxResidence = TaxResidence(country: 'USA', id: '123');
-
-      expect(
-        taxResidence.toString(),
-        equals('TaxResidence{country: USA, id: 123}'),
-      );
+      expect(taxResidence.country, equals(country));
+      expect(taxResidence.id, equals(id));
     });
   });
 }
