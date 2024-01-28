d5b4bc5c8917ebeac4fd281d0c20c949da1c7a61
GianMen91 <giancarlo.mennillo@gmail.com>
2024-01-28 03:24:49 +0100

    add comments to the code

--- lib/models/tax_residence.dart
+++ lib/models/tax_residence.dart
@@ -1,11 +1,12 @@
+// Class representing a TaxResidence item
 class TaxResidence {
   // Properties of a TaxResidence item
-  String country;
-  String id;
+  String country; // The country associated with the tax residence
+  String id; // Identifier for the tax residence
 
   // Constructor for creating a TaxResidence item
   TaxResidence({
-    required this.country,
-    required this.id,
+    required this.country, // Country is required for initialization
+    required this.id, // ID is required for initialization
   });
 }
